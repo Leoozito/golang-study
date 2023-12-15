1617e992f06ad3ba0f77d861d0eacff8ec4f7c2d
Leoozito <leonardonogueira616@gmail.com>
2023-12-15 12:35:55 -0300

    Adicionando o estudo de arrays em go, e feita uma organização de pasta

---
+++ alura/01.linguagem_google/topicos_resumo/07.array.go
@@ -0,0 +1,22 @@
+// Quando trabalhamos com GO, normalmente não usamos ARRAY's, e sim SLICES.
+// pois o ARRAY ele tem que ter uma certa quantidade de armazenamento de dados, assim quando definimos
+
+package main
+import "fmt"
+
+func main() {
+	resp = lista()
+	fmt.Println(resp)
+}
+
+func lista() {
+	// criando a lista
+	var lista [3]string
+
+	// adicionando itens na lista
+	lista[0] = "Leonardo"
+	lista[1] = "Laís"
+	lista[2] = "Vitoria"
+
+	return lista
+}
